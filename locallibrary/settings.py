be50fd2c86384d131da1d6f72f6ab2fe3b91bfb4
KaiqueTI <kaiquewatanabeti@gmail.com>
2021-08-14 14:54:57 -0300

    Update settings.py

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -29,7 +29,11 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 # DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '0.0.0.0'
+    'the-yogify.herokuapp.com',
+    '127.0.0.1'
+]
 
 
 # Application definition
